c88503ff5282fc7bd320058c6a3a83e9d0e8fcf8
demo <kavyashreek025@gmail.com>
2026-05-07 12:31:26 +0530

    Day 2 database and models setup

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+# ---------------- USER TABLE ----------------
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
+
+# ---------------- PRODUCT TABLE ----------------
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Float)
+    stock = db.Column(db.Integer)
 
 @app.route('/')
 def home():
-    return "My E-Commerce API is running!"
+    return "Database connected successfully!"
+
+# Create database tables
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
